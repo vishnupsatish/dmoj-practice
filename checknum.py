a7b32d2541197fb9b78961769a52713ad1413d9b
Vishnu S <vishnupavan.satish@gmail.com>
2021-11-30 08:21:34 -0500

    Update checknum.py

--- checknum.py
+++ checknum.py
@@ -1,15 +1,25 @@
 from math import sqrt, ceil
 from sys import stdin
 
-# Apparently sqrt shouldn't pass
-
 input = stdin.readline
 
 n = int(input())
 
 for e in range(n):
     a = int(input())
-    squ = int(sqrt(a))
+
+    l = 0
+    r = 1000000003
+    
+    # Binary search for sqrt
+    while l < r:
+        mid = (l + r) // 2
+        if mid * mid > a:
+            r = mid
+        else:
+            l = mid + 1
+    squ = l-1
+
     req_from_squ = ceil(squ / 9)
     rem = a - squ * squ
     if (ceil(rem / 9)) > req_from_squ:
